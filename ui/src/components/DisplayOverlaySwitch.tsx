157cb1ed8272b34850d8d9bb7a5b966ab1e3c846
Joel Seidel <joeldseidel@gmail.com>
2020-06-18 18:19:21 -0400

    Created visual overlay control switch

---
+++ ui/src/components/DisplayOverlaySwitch.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import { Form } from 'react-bootstrap';
+
+export interface DisplayOverlaySwitchProps {
+    title : string;
+    name: string;
+    onChange(key: string, value: boolean) : any;
+}
+
+export class DisplayOverlaySwitch extends React.Component<DisplayOverlaySwitchProps, {}> {
+    state = {
+        checked: false
+    };
+    constructor(props: DisplayOverlaySwitchProps){
+        super(props);
+        this.state = {
+            checked: false
+        };
+        this.handleToggle=this.handleToggle.bind(this);
+    }
+    handleToggle(){
+        this.state.checked = !this.state.checked;
+        this.props.onChange(this.props.name, this.state.checked)
+    }
+    render(){
+        const titleLabel : string = "Show " + this.props.title;
+        return (
+            <Form.Check type="switch" label={titleLabel} id={this.props.title} onChange={this.handleToggle} />
+        );
+    }
+}
