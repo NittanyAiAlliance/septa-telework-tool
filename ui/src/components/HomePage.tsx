4282e04bed94898fb3e0d6bd869c5b80a8405749
Joel Seidel <joeldseidel@gmail.com>
2020-06-18 21:30:37 -0400

    Update home page to reflect base styling

--- ui/src/components/HomePage.tsx
+++ ui/src/components/HomePage.tsx
@@ -10,11 +10,14 @@ export class HomePage extends React.Component<HomePageProps, {}> {
         return(
             <main>
                 <Jumbotron fluid>
-                    <Container>
+                    <Container fluid>
                         <h1>SEPTA Future Telework Forecasting Tool</h1>
                     </Container>
                 </Jumbotron>
-                <MapView />
+                <Container fluid className="p-0 m-0">
+                    <h3 className="top-bottom-grey-border pt-3 pb-3 pl-0 pr-0">Philadelphia Telework Forecasting Map</h3>
+                    <MapView />
+                </Container>
                 <Footer />
             </main>
         );
