627db862ca1eeeb31539b2db5481c1613477c5e7
Joel Seidel <joeldseidel@gmail.com>
2020-06-18 21:30:04 -0400

    Update footer to look less awful

--- ui/src/components/Footer.tsx
+++ ui/src/components/Footer.tsx
@@ -1,16 +1,16 @@
 import * as React from 'react';
-import { Container } from 'react-bootstrap';
+import { Container, Jumbotron } from 'react-bootstrap';
 
 export interface FooterProps {}
 
 export class Footer extends React.Component<FooterProps, {}> {
     render() {
         return (
-            <footer>
-                <Container fluid>
+            <Jumbotron>
+                <Container fluid className="mb-sm-5">
                     <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Curabitur eu interdum sem. Mauris sit amet tempus libero, at feugiat nulla.</p>
                 </Container>
-            </footer>
+            </Jumbotron>
         );
     }
 }
