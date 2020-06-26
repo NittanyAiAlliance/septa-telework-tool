664d0ea757b11f6284e578e5d305d02ac1a38e9a
Joel Seidel <joeldseidel@gmail.com>
2020-06-26 19:40:03 -0400

    Add transit line overlay dialog to map controls

--- ui/src/components/MapControls.tsx
+++ ui/src/components/MapControls.tsx
@@ -1,9 +1,12 @@
 import * as React from 'react';
 import { Form, Container } from 'react-bootstrap';
 import {DisplayOverlaySwitch} from "./DisplayOverlaySwitch";
+import {TransitOverlaySelector} from "./TransitOverlaySelector";
+import {Map as LeafletMap} from "react-leaflet";
 
 export interface MapControlsProps {
-    onDisplayChange(key: string, value: boolean): any
+    onDisplayChange(key: string, value: boolean): any,
+    onRouteOverlayChange(routeName : string, newIsVisible : boolean): any,
 }
 
 export class MapControls extends React.Component<MapControlsProps, {}> {
@@ -14,6 +17,7 @@ export class MapControls extends React.Component<MapControlsProps, {}> {
                     <DisplayOverlaySwitch title="Regions" name="regions" onChange={this.props.onDisplayChange} />
                     <DisplayOverlaySwitch title="Transit Lines" name="transit" onChange={this.props.onDisplayChange} />
                     <DisplayOverlaySwitch title="Census Tracts" name="census" onChange={this.props.onDisplayChange} />
+                    <TransitOverlaySelector onUpdate={this.props.onRouteOverlayChange}/>
                 </Form>
             </Container>
         );
